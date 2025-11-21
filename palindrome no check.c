9f1f2de45b84600f835db44e21ccdd0c722b643d
param-samsung <patelparam0703@gmail.com>
2025-11-21 10:58:48 +0530

    Update palindrome no check.c

--- palindrome no check.c
+++ palindrome no check.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-int reverseNumber(int num) {
-    int rev = 0, sign = (num < 0) ? -1 : 1;
-    num = (num < 0) ? -num : num;
-
-    while (num) {
-        rev = rev * 10 + num % 10;
-        num /= 10;
-    }
-    return sign * rev;
-}
 
 int main() {
-    int number;
-    scanf("%d", &number);
-
-    if (number == reverseNumber(number))
-        printf("Palindrome\n");
-    else
-        printf("Not a Palindrome\n");
-
-    return 0;
-}
+    int num,digits=0,numc;
+    scanf("%d",&num);
+    numc=num;
+    while(numc!=0){
+        numc=numc/10;
+        digits++;
+    }
+    int n1[digits];
+    int i=0;
+    while(num!=0){
+        n1[i]=num%10;
+        num=num/10;
+        i++;
+    }
+    int flag=1;
+    for(int i=0;i<digits;i++){
+        if(n1[i]!=n1[digits-i-1]){
+            flag=0;
+        }
+    }
+    printf("%d",flag);}
